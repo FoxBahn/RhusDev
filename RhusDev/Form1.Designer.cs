159c2936103b8fa5456eace01a10469cc34c38c5
CroucampCC <70951041+CroucampCC@users.noreply.github.com>
2020-10-07 12:27:02 +0200

    HEllo
    lekker button

--- RhusDev/Form1.Designer.cs
+++ RhusDev/Form1.Designer.cs
@@ -29,6 +29,7 @@
 		private void InitializeComponent()
 		{
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,15 +42,27 @@
             this.label1.Text = "CMPG223 Taak";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(255, 134);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(913, 487);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Login";
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,6 +71,7 @@
 		#endregion
 
 		private System.Windows.Forms.Label label1;
-	}
+        private System.Windows.Forms.Button button1;
+    }
 }
 
--- RhusDev/Form1.cs
+++ RhusDev/Form1.cs
@@ -21,5 +21,15 @@ namespace RhusDev
 		{
 
 		}
-	}
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
